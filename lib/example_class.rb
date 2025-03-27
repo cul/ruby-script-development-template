d94b329db924628e8c379040c04ce0a94f1783c9
Eric O <eric@ejdigitalmedia.com>
2025-03-27 16:52:25 -0400

    Initial commit

---
+++ lib/example_class.rb
@@ -0,0 +1,9 @@
+class ExampleClass
+  def example_instance_method
+    SomeNamespace::AnotherExampleClass.example_instance_method
+  end
+
+  def self.example_class_method
+    puts 'Hello again!'
+  end
+end
---
+++ lib/some_namespace/another_example_class.rb
@@ -0,0 +1,5 @@
+class SomeNamespace::AnotherExampleClass
+  def self.example_instance_method
+    puts 'Hello!'
+  end
+end
